5353e63cd75fec3f95a87e79477164facdefc09d
Segun Ogundipe <davephenoms@gmail.com>
2019-05-29 11:21:25 +0100

    chore(car):create car response model [Start #166323673]

---
+++ server/models/carModel.js
@@ -0,0 +1,86 @@
+export default class Car {
+  constructor(id, owner, state, status, price,
+    manufacturer, model, bodyType) {
+    this.id = id;
+    this.owner = owner;
+    this.createdOn = new Date().toLocaleString();
+    this.state = state;
+    this.status = status;
+    this.price = price;
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.bodyType = bodyType;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getOwner() {
+    return this.owner;
+  }
+
+  getCreatedOn() {
+    return this.createdOn;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  getPrice() {
+    return this.price;
+  }
+
+  getManufacturer() {
+    return this.manufacturer;
+  }
+
+  getModel() {
+    return this.model;
+  }
+
+  getBodyType() {
+    return this.bodyType;
+  }
+
+  setId(id) {
+    this.id = id;
+  }
+
+  setOwner(owner) {
+    this.owner = owner;
+  }
+
+  setCreatedOn(createdOn) {
+    this.createdOn = createdOn;
+  }
+
+  setState(state) {
+    this.state = state;
+  }
+
+  setStatus(status) {
+    this.status = status;
+  }
+
+  setPrice(price) {
+    this.price = price;
+  }
+
+  setManufacturer(manufacturer) {
+    this.manufacturer = manufacturer;
+  }
+
+  setModel(model) {
+    this.model = model;
+  }
+
+  setBodyType(bodyType) {
+    this.bodyType = bodyType;
+  }
+}
---
+++ server/models/carResponse.js
@@ -0,0 +1,87 @@
+import { create } from "domain";
+
+export default class CarResponse {
+  constructor(Car, User) {
+    this.id = Car.getId();
+    this.email = User.getEmail();
+    this.createdOn = Car.getCreatedOn();
+    this.manufacturer = Car.getManufacturer();
+    this.model = Car.getModel();
+    this.price = Car.getPrice();
+    this.state = Car.getState();
+    this.status = Car.getStatus();
+    this.bodyType = Car.getBodyType();
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getCreatedOn() {
+    return this.createdOn;
+  }
+
+  getManufacturer() {
+    return this.manufacturer;
+  }
+
+  getModel() {
+    return this.model;
+  }
+
+  getPrice() {
+    return this.price;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  getBodyType() {
+    return this.bodyType;
+  }
+
+  setId(id) {
+    this.id = id;
+  }
+
+  setEmail(email) {
+    this.email = email;
+  }
+
+  setCreatedOn(createdOn) {
+    this.createdOn = createdOn;
+  }
+
+  setManufacturer(manufacturer) {
+    this.manufacturer = manufacturer;
+  }
+
+  setModel(model) {
+    this.model = model;
+  }
+
+  setPrice(price) {
+    this.price = price;
+  }
+
+  setState(state) {
+    this.state = state;
+  }
+
+  setStatus(status) {
+    this.status = status;
+  }
+
+  setBodyType(bodyType) {
+    this.bodyType = bodyType;
+  }
+}
