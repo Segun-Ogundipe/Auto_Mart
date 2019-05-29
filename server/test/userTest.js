e3b8ec5d1e0e58f534eaf9d9620124c705f9cfd5
Segun Ogundipe <davephenoms@gmail.com>
2019-05-29 08:11:49 +0100

    test(user):add test case for sign in endpoint

--- server/test/userTest.js
+++ server/test/userTest.js
@@ -6,12 +6,12 @@ import chaiHttp from 'chai-http';
 chai.use(chaiHttp);
 const { expect } = chai;
 
-describe('USER ROUTE TEST', () => {
+describe('SIGNUP ROUTE', () => {
   describe('POST 400', () => {
     it('should have a status of 400', (done) => {
       chai.request('127.0.0.1:3000').post('/api/v1/auth/signup')
         .end((err, res) => {
-          expect(res.status).to.equal(400);
+          expect(res.body.status).to.equal(400);
           done();
         });
     });
@@ -32,7 +32,51 @@ describe('USER ROUTE TEST', () => {
       chai.request('127.0.0.1:3000').post('/api/v1/auth/signup')
         .send(body)
         .end((err, res) => {
-          expect(res.status).to.equal(201);
+          expect(res.body.status).to.equal(201);
+          expect(res.body).to.be.a('object');
+          done();
+        });
+    });
+  });
+});
+
+describe('SIGNIN ROUTE', () => {
+  describe('POST 400', () => {
+    it('should have a status of 400', (done) => {
+      chai.request('127.0.0.1:3000').post('/api/v1/auth/signin')
+        .end((err, res) => {
+          expect(res.body.status).to.equal(400);
+          done();
+        });
+    });
+  });
+
+  describe('POST 422', () => {
+    it('should have a status of 422', (done) => {
+      const body = {
+        email: 'davephen@gmail.com',
+        password: 'jhfdcthjk24r44',
+      };
+      chai.request('127.0.0.1:3000').post('/api/v1/auth/signin')
+        .send(body)
+        .end((err, res) => {
+          expect(res.body.status).to.equal(422);
+          done();
+        });
+    });
+  });
+
+  describe('POST 200', () => {
+    it('should have a status of 200', (done) => {
+      const body = {
+        email: 'davephenom@gmail.com',
+        password: 'jhfdcthjk24r44',
+      };
+
+      chai.request('127.0.0.1:3000').post('/api/v1/auth/signin')
+        .send(body)
+        .end((err, res) => {
+          expect(res.body.status).to.equal(200);
           expect(res.body).to.be.a('object');
           done();
         });
