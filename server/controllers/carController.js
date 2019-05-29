c15e15f8e13fea0ce8926c1557df4e1a9ba22259
Segun Ogundipe <davephenoms@gmail.com>
2019-05-29 13:21:34 +0100

    feature(car):add car endpoint [#166323673]

---
+++ server/controllers/carController.js
@@ -0,0 +1,31 @@
+/* eslint-disable class-methods-use-this */
+import CarQueries from '../queries/carQueries';
+import UserQueries from '../queries/userQueries';
+import validators from '../helpers/validators';
+import Error from '../models/error';
+import cars from '../db/cardb';
+import Success from '../models/success';
+import CarResponse from '../models/carResponse';
+
+const Query = new CarQueries();
+const UserQuery = new UserQueries();
+
+export default class CarController {
+  create(req, res) {
+    const { body } = req;
+    let Car = null;
+    let User = null;
+
+    if (!validators.isValidCar(body)) {
+      res.status(400).json(new Error(400, 'The request body is malformed'));
+    } else {
+      Car = Query.createCar(body, cars);
+      User = UserQuery.findUserById(Car.getOwner());
+      if (User === null) {
+        res.status(400).json(new Error(404, 'The id is not associated with any user'));
+      } else {
+        res.status(201).json(new Success(201, new CarResponse(Car, User)));
+      }
+    }
+  }
+}
