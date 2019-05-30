1c041c20ccba0cd1288f1d333801f73f30944b3d
Segun Ogundipe <davephenoms@gmail.com>
2019-05-30 14:34:11 +0100

    chore(order):create order query [Start #166352964]

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -1,11 +1,11 @@
 export default class Order {
-  constructor(id, buyer, carId, amount, status) {
+  constructor(id, buyer, carId, amount) {
     this.id = id;
     this.buyer = buyer;
     this.createdOn = new Date().toLocaleString();
     this.carId = carId;
     this.amount = amount;
-    this.status = status;
+    this.status = 'pending';
   }
 
   getId() {
