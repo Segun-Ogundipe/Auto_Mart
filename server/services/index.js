fad4336877d0556ce49d1bca00e9b91b1442a7fa
Segun Ogundipe <davephenoms@gmail.com>
2019-06-15 22:18:41 +0100

    chore(database):change saving user data in memory to database
    [Finishes #166716003]

---
+++ server/services/index.js
@@ -0,0 +1,15 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default {
+  query(text, params) {
+    return new Promise((resolve, reject) => {
+      pool.query(text, params)
+        .then(res => resolve(res.rows))
+        .catch(err => reject(err));
+    });
+  },
+};
