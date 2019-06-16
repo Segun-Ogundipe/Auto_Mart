0b889d46c3d57ba07357e6fe80ffb55b2fb58d6b
Segun Ogundipe <davephenoms@gmail.com>
2019-06-16 14:27:05 +0100

    chore(car):add persisting car to database
    [Finishes #166720122]

--- server/models/CarResponse.js
+++ server/models/CarResponse.js
@@ -1,8 +1,7 @@
 export default class CarResponse {
-  constructor(Car, User) {
+  constructor(isUpdate, Car, User) {
     this.id = Car.id;
     this.email = User.email;
-    this.createdOn = Car.createdOn;
     this.manufacturer = Car.manufacturer;
     this.model = Car.model;
     this.price = Car.price;
@@ -10,5 +9,9 @@ export default class CarResponse {
     this.status = Car.status;
     this.bodyType = Car.bodyType;
     this.imageUrl = Car.imageUrl;
+    this.createdOn = Car.createdOn;
+    if (isUpdate === true) {
+      this.updatedOn = Car.updatedOn;
+    }
   }
 }
