8eba78955be10ec39e792a5324f07955711d834e
Segun Ogundipe <davephenoms@gmail.com>
2019-06-20 20:35:35 +0100

    feature(flag):add model class for flags
    [Start #166819708]

---
+++ server/models/FlagModel.js
@@ -0,0 +1,8 @@
+export default class Flag {
+  constructor(id, carId, reason, description) {
+    this.id = id;
+    this.carId = carId;
+    this.reason = reason;
+    this.description = description;
+  }
+}
---
+++ server/services/FlagService.js
@@ -0,0 +1,5 @@
+export default class FlagService {
+  static async createFlag() {
+    const query = 'INSERT INTO flags()';
+  }
+}
