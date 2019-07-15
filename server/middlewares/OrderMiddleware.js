3aeb338732c429004b96ebfaea17ba3ad53378bb
Segun Ogundipe <davephenoms@gmail.com>
2019-07-15 01:28:54 +0100

    chore:gradr test

--- server/middlewares/OrderMiddleware.js
+++ server/middlewares/OrderMiddleware.js
@@ -87,6 +87,7 @@ export default class OrderMiddleware {
 
       next();
     } catch (error) {
+      console.log(error.message)
       res.status(error.status || 500).json(new Response(false, error.status || 500, error.message));
     }
   }
